9c4251a65dfdafdcb1a90d7c82666dc5ad2faa07
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 09:58:07 -0300

    vendor: Enable apex only for supported devices
    Change-Id: If8a38d7bfff45d94d19430167d43fa31fa84dd80
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- config/apex.mk
+++ config/apex.mk
@@ -1,3 +1,12 @@
+ifeq ($(TARGET_FLATTEN_APEX),false)
+
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/descendant/apex/overlay
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/descendant/apex/overlay/common
+
 # Enable Google Play system updates support
 PRODUCT_SOONG_NAMESPACES += \
     vendor/descendant/apex
@@ -24,3 +33,5 @@ PRODUCT_PACKAGES += \
     com.google.android.telephony \
     com.google.android.tzdata2 \
     com.google.android.wifi
+
+endif
