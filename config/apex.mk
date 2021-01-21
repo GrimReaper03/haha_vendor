PixelExperience/vendor_aosp

vendor: Import apex from RD1A.201105.003.C1
Change-Id: Ic74069324343cee7cece4a7a182cf76f35147d3c

config/apex.mk
@@ -1,0 +1,26 @@
+# Enable Google Play system updates support
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/aosp/apex
+
+# ModuleMetadata
+PRODUCT_PACKAGES += \
+    ModuleMetadataGoogle
+
+# Google Apexes
+PRODUCT_PACKAGES += \
+    com.google.android.adbd \
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2 \
+    com.google.android.wifi

config/common.mk
@@ -167,4 +167,7 @@ $(call inherit-product, vendor/aosp/config/carrier_settings.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Apex
+$(call inherit-product, vendor/aosp/config/apex.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

GrimReaper03/haha_vendor

Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

config/apex.mk
@@ -1,0 +1,26 @@
+# Enable Google Play system updates support
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/descendant/apex
+
+# ModuleMetadata
+PRODUCT_PACKAGES += \
+    ModuleMetadataGoogle
+
+# Google Apexes
+PRODUCT_PACKAGES += \
+    com.google.android.adbd \
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2 \
+    com.google.android.wifi

config/common.mk
@@ -164,6 +164,9 @@ PRODUCT_PACKAGES += \
 #Include GApps
 include vendor/gapps/common/common-vendor.mk
 
+# Apex
+$(call inherit-product, vendor/descendant/config/apex.mk)
+
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := false
 ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)